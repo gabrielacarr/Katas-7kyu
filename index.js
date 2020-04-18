ccc1d01640f86962455575a0b8173d8da0600a8b
Gabriela <24256452+gabrielacarr@users.noreply.github.com>
2020-04-18 19:14:23 -0400

    new kata

--- index.js
+++ index.js
@@ -46,4 +46,18 @@ function disemvowel(str) {
   // solution 4.1
     const disemvowel = str => str.split('').filter(letter => !"aeiou".includes(letter.toLowerCase())).join('');
   
-  
+/*
+ * Square Every Digit
+ * https://www.codewars.com/kata/546e2562b03326a88e000020
+ */
+// Solution 1
+// toString - https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Object/toString
+// join - https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/join 
+function squareDigits(num){
+    let string = num.toString();  // turn number to a string
+    let results = [];             // create an array to save the new values of the string
+    for (let i = 0; i < string.length; i++){  // iterate through the string
+        results[i] = string[i] * string[i];   // save the square of the number to the array 
+    }
+    return Number(results.join(''));    // turn the array into a string and then into a number
+  }
