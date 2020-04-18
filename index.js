5b81cf3cb0caccd14ef41985e04862f155278464
Gabriela <24256452+gabrielacarr@users.noreply.github.com>
2020-04-18 19:12:23 -0400

    third

--- index.js
+++ index.js
@@ -8,6 +8,20 @@ function disemvowel(str) {
       return str;
     }
 
-    // solution 2
+// solution 2
   const disemvowel = (str) => str.replace(/[aeiou]/gi, "");
   
+// solution 3
+  // charAt - https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/String/charAt
+  // indexOf - https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/indexOf
+  // toLowercase - https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/String/toLowerCase
+  const disemvowel = (str) => {
+    let vowels = ['a', 'e', 'i', 'o', 'u'];
+    let newStr = '';
+    
+    for (let i = 0; i <= str.length; i++) { // interate through the string
+      if (vowels.indexOf(str.charAt(i).toLowerCase()) == -1)  // check if the current character exist in the vowels array
+        newStr += str.charAt(i);       // if it doesn't exist then add it to the new string
+    }
+    return newStr;
+  }
