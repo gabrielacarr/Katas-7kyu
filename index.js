1adbc7094dd6c2c85564e13c30fc4f75b3272465
Gabriela <24256452+gabrielacarr@users.noreply.github.com>
2020-04-18 19:15:19 -0400

    three

--- index.js
+++ index.js
@@ -75,4 +75,15 @@ function squareDigits(num){
     return parseInt(array.join("")); // turn the array into a string and then a number
   }
 
-  
+ // Solution 3 
+function squareDigits(num){
+    let result = num
+      .toString()                     // turn number to string
+      .split("")                      // turn string into array
+      .map( num => parseInt(num) )    // map each index and return a number, now you have a number array
+      .map( num => num * num )        // map the number array and return the square of each number
+      .join("")                       // turn the number array into a string
+     
+    return parseInt(result)           // turn the string into a number
+  }
+   
