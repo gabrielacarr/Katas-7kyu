9747946625d3117354d9f72e864604283d248490
Gabriela <24256452+gabrielacarr@users.noreply.github.com>
2020-04-18 19:14:54 -0400

    two

--- index.js
+++ index.js
@@ -61,3 +61,18 @@ function squareDigits(num){
     }
     return Number(results.join(''));    // turn the array into a string and then into a number
   }
+
+// Solution 2 
+// parseInt - https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/parseInt 
+// split - https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/String/split 
+// map - https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/map  
+function squareDigits(num){
+    let array = num.toString().split('').map( function(int) {   // turn number to string then make string to array then use map to return the square of each number
+      let i = parseInt(int);
+      return i * i;
+    });
+    
+    return parseInt(array.join("")); // turn the array into a string and then a number
+  }
+
+  
